7b0fdc7b659e714ef6bc339b26684f65f5f9c5a7
yhlee214 <113602114+yhlee214@users.noreply.github.com>
2024-01-16 22:48:29 +0900

    240115 실패...

---
+++ younghyo/Solution240115.java
@@ -0,0 +1,53 @@
+/* 240115 혼자 놀기 달인 (실패...)*/
+
+
+import java.util.*;
+
+class Solution240115 {
+
+	public static void main(String[] args) {
+
+		Solution240115 t = new Solution240115();
+		int cards[] = new int[100];
+
+		t.solution(cards);
+	}
+
+	public int solution(int[] cards) {
+
+		Scanner scn = new Scanner(System.in);
+
+		int i;
+		int n = scn.nextInt();
+	
+		
+		Integer[] box = new Integer[n];
+
+		int answer = 0;
+
+		for (i = 0; i < n; i++) {
+			cards[i] = i + 1;
+			box[i] = cards[i];
+		}
+
+		List<Integer> list = Arrays.asList(box);
+
+		Collections.shuffle(list);
+		list.toArray(box);
+		
+				
+		for (i = 0; i < box.length; i++) {
+			if (i == 0) {
+				System.out.print("[ " + box[i] + ", ");
+			} else if (i == box.length - 1) {
+				System.out.print(box[i] + " ]");
+			} else {
+				System.out.print(box[i] + ", ");
+			}		
+			
+		}
+		
+		
+		return answer;
+	}
+}
