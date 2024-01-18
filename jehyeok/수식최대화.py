ffe25f4f63836d65a2671f42d92745e615cf3362
Yun-Jehyeok <54862291+Yun-Jehyeok@users.noreply.github.com>
2024-01-18 18:12:39 +0900

    1/18

--- jehyeok/수식최대화.py
+++ jehyeok/수식최대화.py
@@ -32,6 +32,7 @@ expression = "100-200*300-500+20"
 
 #     return answer
 
+# 풀다가 다 못풀었음
 def solution(expression):
     answer = []
 
@@ -75,3 +76,17 @@ def solution(expression):
 
 
 print(solution(expression))
+
+# 정답
+def solution(expression):
+    operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
+    answer = []
+    for op in operations:
+        a = op[0]
+        b = op[1]
+        temp_list = []
+        for e in expression.split(a):
+            temp = [f"({i})" for i in e.split(b)]
+            temp_list.append(f'({b.join(temp)})')
+        answer.append(abs(eval(a.join(temp_list))))
+    return max(answer)
