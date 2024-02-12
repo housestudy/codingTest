a824cf4f7399087b968ce8e687d80291b2605889
Yun-Jehyeok <54862291+Yun-Jehyeok@users.noreply.github.com>
2024-02-12 17:25:33 +0900

    2/12

---
+++ jehyeok/1865.py
@@ -0,0 +1,36 @@
+# 내 답
+import sys
+INF = int(1e9)
+input = sys.stdin.readline
+
+def solution(N, distance, graph):
+    distance[1] = 0
+    for check in range(N):
+        for vertex in range(1, N+1):
+            for next_vertex, next_cost in graph[vertex]:
+                if distance[next_vertex] > distance[vertex] + next_cost:
+                    distance[next_vertex] = distance[vertex] + next_cost
+                    if check == N-1:
+                        return False
+    return True
+
+TC = int(input())
+for test_case in range(TC):
+    N, M, W = map(int, input().split())
+
+    graph = [[] for _ in range(N+1)]
+    distance = [INF] * (N+1)
+
+    for _ in range(M):
+        S, E, T = map(int, input().split())
+        graph[S].append([E,T])
+        graph[E].append([S,T])
+
+    for _ in range(W):
+        S, E, T = map(int, input().split())
+        graph[S].append([E,-T])
+
+    if solution(N, distance, graph):
+        print("NO")
+    else:
+        print("YES")
