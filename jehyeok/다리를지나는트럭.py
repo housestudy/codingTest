20ded026d29d0d886b9e2c11907649551148ccb3
Yun-Jehyeok <54862291+Yun-Jehyeok@users.noreply.github.com>
2024-02-24 11:47:33 +0900

    2/23

--- jehyeok/다리를지나는트럭.py
+++ jehyeok/다리를지나는트럭.py
@@ -42,3 +42,28 @@ def solution(bridge_length, weight, truck_weights):
     return answer
 
 print(solution(bridge_length, weight, truck_weights))
+
+# 답
+# 어렵게 생각할 이유도 없었고~ 수학적인 사고는 뭔 쓰잘데기 없는 짓이냐~
+def solution(bridge_length, weight, truck_weights):
+    
+    time = 0
+    bridge = deque([0] * bridge_length)
+    truck_weights = deque(truck_weights)
+    
+    currentWeight = 0
+    while len(truck_weights) != 0:
+        time += 1
+
+        currentWeight -= bridge.popleft()
+
+        if currentWeight + truck_weights[0] <= weight:
+            currentWeight += truck_weights[0]
+            bridge.append(truck_weights.popleft())
+
+        else: 
+            bridge.append(0)
+            
+    time += bridge_length
+    
+    return time
